21de6f06657f322398fddcce7d5d66df04abac3b
Marin4ik225 <Marin4ik225@gmail.com>
2025-05-03 02:53:22 +0500

    выполнение задания в части js

---
+++ js/hw-2.js
@@ -0,0 +1,77 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+//Задание 2 (9 января 2007г)
+let year_iPhone = "9 января 2007г";
+alert("год выпуска первого iPhone: " + year_iPhone);
+
+//Задание 3 (Стив Джобс)
+let name_js = "Стив Джобс";
+alert("имя создателя языка JavaScript: " + name_js);
+
+//Задание 4
+let per1 = 10;
+let per2 = 2;
+// Сложение (+):
+let sum = per1 + per2;
+alert("Сумма: " + sum);
+
+// Вычитание (-):
+let difference = per1 - per2;
+alert("Вычитание: " + difference);
+
+// Умножение (*):
+let product = per1 * per2;
+alert("Умножение: " + product);
+
+// Деление (/):
+let quotient = per1 / per2;
+alert("Деление: " + quotient);
+
+
+//Задание 5
+// Показатель степени (**):
+let power = 2 ** 5;
+alert("Показатель степени: " + power);
+
+//Задание 6
+// Модуль от деления (%), остаток от деления:
+a = 9;
+let b = 2;
+let remainder = a % b;
+alert("Модуль от деления: " + remainder);
+
+//Задание 7
+let num = 1;
+// num = num + 5;
+num += 5;
+// num = num - 3;
+num -= 3;
+// num = num * 7;
+num *= 7;
+// num = num / 3;
+num /= 3;
+// num = num + 1;
+num += 1;
+// num = num - 1;
+num -= 1;
+alert("Результат: " + num);
+
+
+//Задание 8
+let age = prompt("Сколько вам лет?");
+alert("Ваш возраст: " + age + " лет");
+
+//Задание 9
+let user = {
+    name: "Nika",
+    age: 10,
+    isAdmin: false
+};
+
+//Задание 10
+let user_name=prompt("Назовите своё имя:");
+alert("Привет, " + user_name + "!");
